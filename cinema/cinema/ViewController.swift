8700abe5a17b953fef76fa6ed3231786ab573c37
applTv <73424904+applTv@users.noreply.github.com>
2022-12-20 11:24:37 +0300

    Add files via upload

---
+++ cinema/cinema/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cinema
+//
+//  Created by Кулешова Анна Викторовна on 01/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
